3e1f5acc581ad47afb6ff96a840ba8b02185eb3c
Mario Barrios <mbarrioos05@gmail.com>
2026-03-23 03:12:18 +0100

    Añadir src/App.jsx inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>Mi Portfolio</h1>
+    </div>
+  )
+}
+
+export default App
+```
+
+Y esto en `.env.example`:
+```
+PORT=3000
+NODE_ENV=development
